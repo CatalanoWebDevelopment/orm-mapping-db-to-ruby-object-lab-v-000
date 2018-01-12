344221243a48a6ceb1273c0b209ad83ef0cfba9a
Parker Catalano <catalano.work@gmail.com>
2018-01-12 19:59:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,6 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10 ORDER BY grade LIMIT 1
     SQL
-    binding.pry
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
