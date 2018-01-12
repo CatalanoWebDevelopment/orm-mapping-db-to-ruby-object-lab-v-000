07c9e7189f81238401a38063de98f64612ffade5
Parker Catalano <catalano.work@gmail.com>
2018-01-12 20:04:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,11 +85,17 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10 ORDER BY grade LIMIT 1
     SQL
-    
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
 
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade)
 
 end
