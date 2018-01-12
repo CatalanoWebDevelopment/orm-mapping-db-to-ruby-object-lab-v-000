fdccbf5db7e113c9fda3c5053ea74e13c39af53d
Parker Catalano <catalano.work@gmail.com>
2018-01-12 19:53:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,5 +79,13 @@ class Student
     DB[:conn].execute(sql, limit)
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
 
 end
